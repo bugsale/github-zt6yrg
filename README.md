df1d851afed540486cf9f5e0ce85bcab690a7d00
bugsale <166558655+bugsale@users.noreply.github.com>
2024-04-09 23:03:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-zt6yrg
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/bugsale/github-zt6yrg)
---
+++ src/app.component.ts
@@ -0,0 +1,18 @@
+
+
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.css'],
+  template: `<ejs-filemanager id='default-filemanager' #filemanagerObj [ajaxSettings]='ajaxSettings' [view]='view'>
+  </ejs-filemanager>`
+})
+export class AppComponent {
+  public view?: any;
+  public hostUrl: string = 'https://ej2-aspcore-service.azurewebsites.net/';
+  public ajaxSettings: object = {
+    url: this.hostUrl + 'api/FileManager/FileOperations'
+  };
+}
+
+
